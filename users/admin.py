6d87a90c67d1a335d1d2ff9601734e21e5693b11
PilYeong <pilyeooong@gmail.com>
2020-01-23 23:07:09 +0900

    ADD: 유저 모델과 어드민페이지 추가

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from users.models import User
 
-# Register your models here.
+class UserAdmin(BaseUserAdmin):
+    model = User
+    fieldsets = BaseUserAdmin.fieldsets + (
+        ('추가 정보', {'fields': ('profile_image', 'gender', 'introduction')}),
+    )
+    list_display = ["username", "email", "profile_image", "gender"]
+
+admin.site.register(User, UserAdmin)
--- users/models.py
+++ users/models.py
@@ -2,4 +2,17 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    pass
+    GENDER_MALE = 'male'
+    GENDER_FEMALE = 'female'
+    GENDER_OTHER = 'other'
+    CHOICES_GENDER = (
+        (GENDER_MALE, '남성'),
+        (GENDER_FEMALE, '여성'),
+        (GENDER_OTHER, '기타'),
+    )
+    profile_image = models.ImageField(upload_to='profile', blank=True)
+    gender = models.CharField(max_length=10, choices=CHOICES_GENDER)
+    introduction = models.TextField(blank=True)
+    
+    def __str__(self):
+        return self.username
