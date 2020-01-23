c0390f6da8b80b36b1516acff6b66afe073c0957
PilYeong <pilyeooong@gmail.com>
2020-01-24 00:17:51 +0900

    FIXED: gitignore 수정

--- users/admin.py
+++ users/admin.py
@@ -4,11 +4,13 @@ from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 User = get_user_model()
 
+
 class UserAdmin(BaseUserAdmin):
     model = User
     fieldsets = BaseUserAdmin.fieldsets + (
         ('추가 정보', {'fields': ('profile_image', 'gender', 'introduction')}),
     )
     list_display = ['username', 'email', 'profile_image', 'gender']
+
 
 admin.site.register(User, UserAdmin)
--- users/models.py
+++ users/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+
 class User(AbstractUser):
     GENDER_MALE = 'male'
     GENDER_FEMALE = 'female'
@@ -13,6 +14,6 @@ class User(AbstractUser):
     profile_image = models.ImageField(upload_to='profile', blank=True)
     gender = models.CharField(max_length=10, choices=CHOICES_GENDER)
     introduction = models.TextField(blank=True)
-    
+
     def __str__(self):
         return self.username
